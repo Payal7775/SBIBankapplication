7f2b0d45190449940bbc76b9c03bd8198b61fd0b
Payal7775 <payaltha008@gmail.com>
2026-01-11 10:00:08 +0530

    initial commit

---
+++ TestBankApplication/src/com/soft/Demo.java
@@ -0,0 +1,11 @@
+package com.soft;
+
+import java.security.DomainCombiner;
+
+public class Demo {
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to java programming....!");
+	}
+
+}
